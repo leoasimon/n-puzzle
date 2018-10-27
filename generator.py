c7aef1800ea7fd6185a5ed729b6e0df37ce8696c
Elizabeth KELEN <ekelen@e1r7p4.42.fr>
2018-10-27 12:12:54 +0200

    generator ok I think

--- generator.py
+++ generator.py
@@ -2,20 +2,58 @@
 
 from collections import deque
 from printer import print_grid 
+import sys
 
+def make_loop(grid, size):
+    i = 1
+    j = size
+    max_val = (size * size) - 1
+    sx = 0
+    sy = 0
+    sv = 1
 
-def fill_top(grid, n, sx, sy, sv):
-    for i in range(sx, sx + n):
-        grid[sy][i] = sv
-        sv += 1
-    return grid
+    sides = ['top', 'right', 'bottom', 'left']
+    side_n = 0
+    while (i <= max_val and j >= 1):
+        if (side_n == 0):
+            # make top
+            for k in range(sx, sx + j):
+                grid[sy][k] = i
+                i += 1
+            side_n = (side_n + 1) % len(sides)
+            sx += j - 1
+            sy += 1
+            j -= 1
+        elif side_n == 1:
+            # make right
+            for k in range(sy, sy + j):
+                grid[k][sx] = i
+                i += 1
+            side_n = (side_n + 1) % len(sides)
+            sx -= 1
+            sy += j - 1
+        elif side_n == 2:
+            # make bottom
+            for k in range(sx, sx - j, - 1):
+                grid[sy][k] = i
+                i += 1
+            side_n = (side_n + 1) % len(sides)
+            sx -= j - 1
+            sy -= 1
+            j -= 1
+        else: # side_n == 3
+            # make left
+            for k in range(sy, sy - j, - 1):
+                grid[k][sx] = i
+                i += 1
+            side_n = (side_n + 1) % len(sides)
+            sx += 1
+            sy -= j - 1
+    return
 
 def make_goal(size):
     max_val = (size * size) - 1
     grid = [[0 for i in range(size)] for x in range(size)]
-    
-    print_grid(grid)
-
-if __name__ == "__main__":
-    make_goal(3)
+    make_loop(grid, size)
+    return grid
 
