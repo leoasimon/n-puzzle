eb1b3a5dbd9135aeb619a6b879a2930455661333
Elizabeth Kelen <elizabethkelen@liz.home>
2018-10-29 09:38:02 +0100

    remove bad manhattan comment

--- heuristics.py
+++ heuristics.py
@@ -13,16 +13,3 @@ def get_manhattan(grid, goal, goal_dict, size): # TODO: Haven't tested
 def get_h_score(grid, goal, goal_dict, size):
 	# Todo: chose one heuristic, default manhattan
 	return get_manhattan(grid, goal, goal_dict, size)
-
-
-# def get_manhattan_2(prev_h, val, old_pos, new_pos, goal, goal_dict, size):
-# 	goal_pos_x, goal_pos_y = goal_dict[val]
-# 	old_pos_x, old_pos_y = goal_dict[val]
-# 	new_pos_x, new_pos_y = new_pos
-# 	prev_h -= abs(old_pos_x - goal_pos_x) + abs(old_pos_y - goal_pos_y)
-# 	h = prev_h + abs(new_pos_x - goal_pos_x) + abs(new_pos_y - goal_pos_y)
-# 	return h
-
-
-# def get_h_score_2(prev_h, val, old_pos, new_pos, goal, goal_dict, size):
-# 	return get_manhattan_2(prev_h, val, old_pos, new_pos, goal, goal_dict, size)
