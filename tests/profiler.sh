26cee04922eaf1acd4bcaf1efdda05a99b74d21d
Elizabeth Kelen <elizabethkelen@liz.local>
2018-11-01 16:50:10 +0100

    add delete option to profiler

--- tests/profiler.sh
+++ tests/profiler.sh
@@ -5,6 +5,8 @@ O_FILE=cprof_output_$(date +%s).cprof
 if [[ -f $1 ]]; then
 	python3 -m cProfile -s cumtime -o $O_FILE ../n-puzzle.py $1
 	snakeviz $O_FILE
+elif [[ $1 == "-d" ]]; then
+	rm -f *.cprof
 else
 	printf "$1 is not a file."
 fi
