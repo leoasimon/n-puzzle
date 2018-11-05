ee251213169bec75a83b610e6bc04e9b9af058e3
Elizabeth Kelen <elizabethkelen@liz.home>
2018-11-05 10:41:47 +0100

    solvable ok

--- solvable.py
+++ solvable.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python3
 
 import numpy as np
-from util import bcolors as col
 from goal import make_goal
 import sys
 
@@ -10,37 +9,32 @@ def _get_inversions(grid1D):
 		return 0
 	inversions = 0
 	for i, a in enumerate(grid1D):
-		for j, b in enumerate(grid1D[i:]):
+		for b in grid1D[i:]:
 			if a > b and a > 0 and b > 0:
 				inversions += 1
 	return inversions
 
 def get_valid(grid, size):
 	# each legal vertical move changes total inversions, horizontal moves do not
-	# if side polarity is odd:
-		# tile X will pass an even no. of tiles if moved vertically
-		# thus adding or subtracting an even number to total inversions
-		# thus polarity of total inversions never changes
-		# thus total inversions polarity MUST match polarity of total inversions at goal state 
-	# if side polarity is even:
-		# tile X will pass an odd no. of tiles if moved vertically
-		# thus changing polarity of total inversions each time
-		# so total inversion polarity if board is legal:
-			# TODO: Placeholder, check if true
-			# 1. MUST match polarity of total inversions at goal state IF blank tile starts AND ends on even row
-			# 2. ELSE must NOT match polarity
+	# vertical moves change polarity of total inversions only if board width is odd
+	# http://www.cs.bham.ac.uk/~mdr/teaching/modules04/java2/TilesSolvability.html
 	grid1D = grid.flatten()
 	inversions_grid = _get_inversions(grid1D)
 
-	size_odd = size & 1
+	side_odd = bool(size & 1)
+	goal = make_goal(size)
 	
-
-	goal1D = make_goal(size).flatten()
+	goal1D = goal.flatten()
 	inversions_goal = _get_inversions(goal1D)
 
-	if not size_odd: 
-		blank_y = size - np.where(grid == 0)[0]
-	return
+	if not side_odd: 
+		blank_row_start = size - np.where(grid == 0)[0]
+		blank_row_goal = size - np.where(goal == 0)[0]
+
+		# compensate for variable goal row polarity in spiral board
+		inversions_goal += blank_row_goal & 1
+		inversions_grid += blank_row_start & 1
+	return bool(inversions_grid & 1) == (inversions_goal & 1)
 	
 if __name__ == "__main__":
 	pass
--- tests/solvable.py
+++ tests/solvable.py
@@ -65,15 +65,53 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-class Solve(TestCase):
+class Valid(TestCase):
 	def setUp(self):
 		pass
 
 	def tearDown(self):
 		pass
 
-	def test_xyz(self):
-		pass
+	def test_solvable_if_side_len_odd_start_blank_odd(self):
+		a = np.array([[8, 1, 3], [6, 2, 7], [0, 4, 5]])
+		self.assertTrue(get_valid(a, 3))
+
+	def test_solvable_if_side_len_odd_start_blank_even(self):
+		a = np.array([[2, 7, 8], [5, 0, 4], [3, 1, 6]])
+		self.assertTrue(get_valid(a, 3))
+
+	def test_solvable_if_side_len_even_goal_blank_even(self):
+		a = np.array([[2, 1, 11, 13], [10, 3, 9, 12], [5, 0, 4, 6], [7, 14, 8, 15]])
+		self.assertTrue(get_valid(a, 4))
+
+	def test_solvable_if_side_len_even_goal_blank_odd(self):
+		a = np.array([[3, 6, 15, 34, 35, 17], [16, 29, 23, 5, 20, 18], [10, 9, 22, 24, 14, 2], [27, 28, 1, 33, 4, 30], [32, 13, 8, 26, 31, 12], [0, 19, 25, 11, 7, 21]])
+		self.assertTrue(get_valid(a, 6))
+
+	def test_solvable_sidelen_even_goalblank_odd_startblank_odd(self):
+		a = np.array([[3, 6, 15, 34, 35, 17], [16, 29, 23, 5, 20, 18], [10, 9, 22, 24, 14, 2], [27, 28, 1, 33, 4, 30], [32, 13, 8, 26, 31, 12], [0, 19, 25, 11, 7, 21]])
+		self.assertTrue(get_valid(a, 6))
+
+	def test_solvable_sidelen_even_goalblank_even_startblank_even(self):
+		a = np.array([[7, 0, 8, 10], [3, 1, 2, 12], [14, 9, 5, 13], [4, 6, 11, 15]])
+		self.assertTrue(get_valid(a, 4))
+
+class Unsolvable(TestCase):
+	def test_unsolvable_sidelen_odd_start_odd(self):
+		a = np.array([[6, 8, 2], [5, 3, 7], [0, 1, 4]])
+		self.assertFalse(get_valid(a, 3))
+
+	def test_unsolvable_sidelen_odd_start_even(self):
+		a = np.array([[8, 2, 1], [7, 0, 4], [6, 5, 3]])
+		self.assertFalse(get_valid(a, 3))
+
+	def test_unsolvable_sidelen_even_start_odd_goal_even(self):
+		a = np.array([[1, 14, 13, 6], [0, 12, 15, 2], [9, 11, 10, 8], [7, 3, 5, 4]])
+		self.assertFalse(get_valid(a, 4))
+
+	def test_unsolvable_sidelen_even_start_even_goal_even(self):
+		a = np.array([[9, 3, 7, 12], [1, 8, 15, 11], [10, 0, 5, 14], [13, 2, 6, 4]])
+		self.assertFalse(get_valid(a, 4))
 
 	
 if __name__ == '__main__':
