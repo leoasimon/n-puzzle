63fc47cdcf181af204ead5da645f1bc769272c3e
failte <lsimon@student.42.fr>
2018-11-06 17:52:16 +0100

    create db: choose goal from json file

--- create_db.py
+++ create_db.py
@@ -3,6 +3,7 @@
 import numpy as np
 import json
 from bfs import bfs
+import sys
 
 def save_json(db, s, n):
 	name = f'{s}x{s}_{n}.json'
@@ -15,28 +16,37 @@ def get_db(s, n):
 	with open(name, "r") as f:
 		return json.load(f)
 
-def get_goals(size):
-	#Todo: handle different sizes
-	a = np.array([
-		[1,2,3],
-		[-1,-1,-1],
-		[-1,-1,-1]
-	])
-	b = np.array([
-		[-1,-1,-1],
-		[8,0,4],
-		[-1,-1,-1]
-	])
-	c = np.array([
-		[-1,-1,-1],
-		[-1,-1,-1],
-		[7,6,5]
-	])
-	return [a,b,c]
+def get_goals(s):
+	with open("goals.json", "r") as f:
+		g_dict = json.load(f)
+		names = [f'{s}x{s}_a', f'{s}x{s}_b', f'{s}x{s}_c']
+		g_list = [np.array(e) for e in [g_dict[n] for n in names]]
+		return g_list
+
+# def get_goals(size):
+# 	#Todo: handle different sizes
+# 	a = np.array([
+# 		[1,2,3],
+# 		[-1,-1,-1],
+# 		[-1,-1,-1]
+# 	])
+# 	b = np.array([
+# 		[-1,-1,-1],
+# 		[8,-1,4],
+# 		[-1,-1,-1]
+# 	])
+# 	c = np.array([
+# 		[-1,-1,-1],
+# 		[-1,-1,-1],
+# 		[7,6,5]
+# 	])
+# 	return [a,b,c]
 
 
 def createDb():
-	size = 3
+	args = sys.argv[1:] if len(sys.argv) >= 2 else []
+	size = 3 if not args else int(args[0])
+	print(size)
 	goals = get_goals(size)
 	for g, name in zip(goals, ['a', 'b', 'c']):
 		db = bfs(g, size, (1,1))
