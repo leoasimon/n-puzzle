e43d3382de18d35ec0ddd684372f079b2960fb48
Elizabeth Kelen <elizabethkelen@liz.local>
2018-11-09 12:24:35 +0100

    tests passing. move numpy heuristics to another file for now.

--- heuristics.py
+++ heuristics.py
@@ -1,64 +1,17 @@
 import numpy as np
 import operator
 
-from bfs import build_key
-from create_db import get_db, get_goals
-
-def get_lc_line(row, goal_row):
-	conflicts = []
-	counts = {}
-	for i, tj in enumerate(row[1:]):
-		if tj not in goal_row:
-			continue
-		for tk in row[0:i+1]:
-			if tk not in goal_row:
-				continue
-			gtk = goal_row.index(tk)
-			gtj = goal_row.index(tj)
-			if gtk > gtj:
-				conflicts.append((tj, tk))
-	for e in row:
-		for c in conflicts:
-			if c[0] == e:
-				if e not in counts:
-					counts[e] = 0
-				counts[e] +=1
-	if not counts:
-		return 0
-	return sum([v - 1 for v in counts.values()]) + 1
+import sys
 
 def get_linear_conflicts(grid, goal, goal_dict, size):
-	#TODO: Needs non-numpy version
-	#TODO: Needs manhattan calc built in
-	diff = 0
-	for i in range(size):
-		col = grid[:,i]
-		row = grid[i,:]
-		goal_col = goal[:,i]
-		goal_row = goal[i,:]
-		diff += get_lc_line(list(row), list(goal_row))
-		diff += get_lc_line(list(col), list(goal_col))
-	return diff * 2
+	sys.exit('LC not implemented without NP yet.')
 
-#Todo: store db somewhere so we don't have to open the file any times
 def get_pattern_cost(grid, size):
-	#TODO: non-numpy
-	diff = 0
-	goals = get_goals(size)
-	for g, name in zip(goals, ['a', 'b', 'c']):
-		nums = [e for e in g.flatten() if e != -1]
-		filtered = np.where(g != 0, grid, -1)
-		key = build_key(filtered, nums)
-		db = get_db(size, name)
-		diff += db[key] if key in db else 0
-	return diff
+	sys.exit('Pattern DB not implemented without NP yet.')
+
 
 def get_misplaced_tiles(grid, goal):
-	#TODO: non-numpy
-	filtered = np.where(grid != goal)
-	if np.where(grid == 0) != np.where(goal == 0):
-		return len(filtered[0]) - 1
-	return len(filtered[0])
+	sys.exit('Misplaced tiles not implemented without NP yet.')
 
 def get_manhattan(grid, goal, goal_dict, size):
 	"""Get manhattan cost (with/without numpy)"""
