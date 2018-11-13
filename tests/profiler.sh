6f2f18c371080ee2d7548d4b3f6f84047ae57389
Elizabeth Kelen <elizabethkelen@liz.local>
2018-11-13 14:07:05 +0100

    update profiler to reflect new flags

--- tests/profiler.sh
+++ tests/profiler.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 interpreter="python3"
-alg="-he=mh"
+he="-he=mh"
 file=''
 oflag='-o '
 o_file=''
+alg='-a=astar'
 visualizer="snakeviz"
 
 usage(){
-	printf "Usage: \n\t$0 puzzlefile [ -he=mh | -he=lc | -he=other algo flag ] [-pypy3]\n\t$0 -d\n\n\t-d: deletes all .cprof files in this directory."
+	printf "Usage: \n\t$0 puzzlefile [ -he=mh | -he=lc | -he=other heuristic flag ] [ -a=greedy ]\n\t$0 -d\n\n\t-d: deletes all .cprof files in this directory."
 	exit 1
 }
 
@@ -27,17 +28,18 @@ fi
 while test $# != 0
 do
     case "$1" in
-    -pypy3) interpreter="pypy3" ;;
     -he=mh|-he=lc|-he=db|-he=mt)
+        he="$1"; shift ;;
+	-a=greedy)
         alg="$1"; shift ;;
     --) shift; break;;
     esac
     shift
 done
 
-o_file=cprof_${file}_alg${alg}_intperpreter-${interpreter}_$(date +%Y-%m-%d_at_%Hh%Ms%S).cprof
+o_file=cprof_${file}_${he}__$(date +%Y-%m-%d_at_%Hh%Ms%S).cprof
 
 command -v $visualizer >/dev/null 2>&1 || { echo >&2 "You don't have snakeviz (pip3 install snakeviz); output will be printed to stdout instead."; o_file=''; oflag=''; visualizer=''; }
 
-$interpreter -m cProfile -s cumtime $oflag$o_file ../n-puzzle.py $fullfile $alg
+$interpreter -m cProfile -s cumtime $oflag$o_file ../n-puzzle.py $fullfile $he $alg
 $visualizer $o_file
