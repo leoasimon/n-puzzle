7eee77f53f9036c332a3ea8a56133539ef84fdf8
Elizabeth Kelen <elizabethkelen@liz.local>
2018-11-14 13:54:59 +0100

    no recursive printer

--- printer.py
+++ printer.py
@@ -2,17 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from collections import deque
 
-def get_path(curr_str, parents, moves):
-	if curr_str is None:
-		return []
-	l = get_path(parents[curr_str], parents, moves + 1)
-	l.append(curr_str)
+def get_path(curr_str, parents):
+	l = []
+	while curr_str is not None:
+		l.append(curr_str)
+		curr_str = parents[curr_str]
 	return l
 
 def print_solution(stats, n_str, parents, i):
-	l = get_path(n_str, parents, 0)
-	# TODO: Format
+	l = get_path(n_str, parents)
+
 	print(f'Heuristic: {stats.heuristic}')
 	print(f'Search type: {stats.algo}')
 	print(f'Found solution in {stats.moves} moves.')
@@ -22,5 +23,4 @@ def print_solution(stats, n_str, parents, i):
 		f'(plus {len(parents) * 3})' # For g_score, f_score, and parents dicts
 	) #TODO: DB?
 
-	#TODO: Print path
 	return l
