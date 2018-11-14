bc6136a692381ea294541db79f375854834cad40
ekelen <ekelen@student.42.us.org>
2018-11-14 17:44:08 +0100

    hmm

--- heuristics.py
+++ heuristics.py
@@ -30,7 +30,7 @@ def get_lc_line(row, goal_row):
 		return 0
 	return sum([v - 1 for v in counts.values()]) + 1
 
-def get_linear_conflicts(grid, goal, goal_dict, size):
+def get_linear_conflicts(grid, goal, goal_dict={}, size=None, dbs=[]):
 	lc = 0
 	m = 0
 	for i in range(size):
@@ -44,10 +44,10 @@ def get_linear_conflicts(grid, goal, goal_dict, size):
 			if val:
 				goal_pos_x, goal_pos_y = goal_dict[str(val)]
 				m += abs(x - goal_pos_x) + abs(i - goal_pos_y)
-	return (m, (lc * 2)) # Keeping separate for testing for now, but adds a few sec to bigger puzzles
+	return m + (lc * 2)
 
 #Todo: store db somewhere so we don't have to open the file any times
-def get_pattern_cost(grid, size, dbs):
+def get_pattern_cost(grid, goal, goal_dict={}, size=None, dbs=[]):
 	diff = 0
 	goals = get_goals(size)
 	for g, db in zip(goals, dbs):
@@ -57,30 +57,28 @@ def get_pattern_cost(grid, size, dbs):
 		diff += db[key] if key in db else 0
 	return diff
 
-def get_misplaced_tiles(grid, goal):
+def get_misplaced_tiles(grid, goal, goal_dict={}, size=None, dbs=[]):
 	filtered = np.where(grid != goal)
 	if np.where(grid == 0) != np.where(goal == 0):
 		return len(filtered[0]) - 1
 	return len(filtered[0])
 
-def get_manhattan(grid, goal, goal_dict, size):
+def get_manhattan(grid, goal, goal_dict, size, dbs=[]):
 	diff = 0
-
 	for y in range(size):
 		for x, val in enumerate(grid[y]):
 			if val:
 				goal_pos_x, goal_pos_y = goal_dict[str(val)]
 				diff += abs(x - goal_pos_x) + abs(y - goal_pos_y)
-	# print(f'diff : {diff}')
 	return diff
 
-def get_h_score(grid, goal, goal_dict, size, options, dbs):
-	if options.heuristic == 'mt':
-		return get_misplaced_tiles(grid, goal)
-	if options.heuristic == 'db':
-		return get_pattern_cost(grid, size, dbs)
-	if options.heuristic == 'lc':
-		m, lc = get_linear_conflicts(grid, goal, goal_dict, size)
-		return m + lc
-	if options.heuristic == 'mh':
-		return get_manhattan(grid, goal, goal_dict, size)
+# def get_h_score(grid, goal, goal_dict, size, options, dbs):
+# 	if options.heuristic == 'mt':
+# 		return get_misplaced_tiles(grid, goal)
+# 	if options.heuristic == 'db':
+# 		return get_pattern_cost(grid, size, dbs)
+# 	if options.heuristic == 'lc':
+# 		m, lc = get_linear_conflicts(grid, goal, goal_dict, size)
+# 		return m + lc
+# 	if options.heuristic == 'mh':
+# 		return get_manhattan(grid, goal, goal_dict, size)
