825de5c8455e8af1dbe378f97cac20442a421b70
Elizabeth KELEN <ekelen@e1r8p5.42.fr>
2018-11-15 10:26:31 +0100

    prettier printing; TODO: greedy move count is wrong

--- printer.py
+++ printer.py
@@ -6,28 +6,45 @@ from collections import deque
 
 from gui import display_all
 
+class C:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def _get_path(curr_str, parents):
 	l = []
 	while curr_str is not None:
 		l.append(curr_str)
 		curr_str = parents[curr_str]
-	return l
+	return list(reversed(l))
 
 def print_solution(stats, n_str, parents, options={}):
-	print(f'Heuristic: {stats.h_name}')
-	print(f'Search type: {stats.algo}')
-	print(f'Found solution in {stats.moves} moves.')
-	print(f'Total number added to open set (time complexity):  {stats.total_open}')
-	print(
-		f'Max nodes in memory (space complexity):  {stats.max_open} '
-		f'(plus {len(parents) * 3})' # For g_score, f_score, and parents dicts
-	) #TODO: DB?
-
 	if options.verbose or options.gui:
 		path = _get_path(n_str, parents)
+
 		if options.verbose:
 			for j, e in enumerate(path):
 				print(f'move: {j}', end=' ')
 				print(e)
-		if options.gui:
-			display_all(stats.size, path)
+
+	print(f'\n--- Found solution in {C.HEADER}{stats.moves}{C.ENDC} moves. ---\n')
+	print(f'Heuristic:                                         {C.BOLD}{stats.h_name}{C.ENDC}')
+	print(f'Search type:                                       {C.BOLD}{stats.algo}{C.ENDC}')
+	print(f'Time complexity:'
+		f'\n\tTotal added to open queue: {C.BOLD}{stats.total_open}{C.ENDC}')
+	print(
+		f'Space complexity:'
+		f'\n\tOpen set max:              {C.BOLD}{stats.max_open}{C.ENDC}'
+		f'\n\tLookup dicts:              {C.BOLD}3 * {len(parents)}{C.ENDC}' # For g_score, f_score, and parents dicts
+	)
+	if options.heuristic == 'db':
+		print(
+		f'\tPattern database:          {3360 + 5765760 + 5765760}')
+	
+	if options.gui:
+		display_all(stats.size, path)
